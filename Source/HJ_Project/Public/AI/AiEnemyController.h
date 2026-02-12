a4094ca237e6fc60ad8de656e3342425c8ac7ab1
0Kaede0 <hun151800@gmail.com>
2026-02-12 17:40:35 +0900

    [성헌호]26/02/12
    ai행동패턴 추가

--- Source/HJ_Project/Public/AI/AiEnemyController.h
+++ Source/HJ_Project/Public/AI/AiEnemyController.h
@@ -3,8 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AIController.h"
+#include "AIController.h" 
+#include "GameFramework/Actor.h" 
 #include "AiEnemyController.generated.h"
+
+
+
+UENUM(BlueprintType)
+enum class EAIState : uint8
+{
+	MovingToGate,//관문으로 이동
+	ChasingPlayer,//플레이어 추격
+	AttackingPlayer, //플레이어 공격
+	Stunned//총맞았을때 경직
+};
+
 
 
 UCLASS(Blueprintable)
@@ -16,6 +29,7 @@ public:
 	AAiEnemyController();
 	
 protected:
+	virtual void BeginPlay() override;
 	virtual void OnPossess(APawn* InPawn) override;
 
 	//추격 갱신
@@ -23,13 +37,33 @@ protected:
 	void UpdateChase();
 
 public:
+	//ai의 상태 저장 변수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	EAIState CurrentState = EAIState::MovingToGate;
+
 	//추격 목표
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	class AActor* TargetPlayer;
 
+	// 최종관문 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	class AActor* FinalGate;
+
+
+	TArray<AActor*> FoundGates;//나중에 최종관문 추가시에 삭제 가능
+
+	//인식거리 설정
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ai")
+	float DetectionRange = 1000.0f;
+
+	//공격 범위 설정값
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float AttackRange = 150.0f;
+
 	//목표가 죽으면 정지 할수 있는 타이머 핸들
 	FTimerHandle ChaseTimerHandle;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "AI")
-	float chaseInterval;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float chaseInterval = 0.2f;
+
 };
