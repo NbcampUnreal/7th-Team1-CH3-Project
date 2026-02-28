54c121fce4a9ec503a76b4c9ff8f86c3dc6b7eda
dksktlq5-pixel <dksktlq5@gmail.com>
2026-02-28 12:09:31 +0900

    [박찬우] 26/02/28
    - 웨이브 추가
    - ABP 수정
    - 총 발사 및 장전

--- Source/HJ_Project/Private/HJ_GameMode.cpp
+++ Source/HJ_Project/Private/HJ_GameMode.cpp
@@ -4,88 +4,102 @@
 #include "HJ_PlayerController.h"
 #include "HJ_SpawnZombie.h"
 #include "Kismet/GameplayStatics.h"
-#include "GameFramework/PlayerController.h"
 #include "TimerManager.h"
 #include "Engine/DataTable.h"
 
 AHJ_GameMode::AHJ_GameMode()
 {
-    GameStateClass = AHJ_GameState::StaticClass();
-    DefaultPawnClass = AHJ_Player::StaticClass();
-    PlayerControllerClass = AHJ_PlayerController::StaticClass();
+	GameStateClass = AHJ_GameState::StaticClass();
+	DefaultPawnClass = AHJ_Player::StaticClass();
+	PlayerControllerClass = AHJ_PlayerController::StaticClass();
 
-    TimeBetweenWaves = 5.f;
+	TimeBetweenWaves = 5.f;
 }
 
 void AHJ_GameMode::BeginPlay()
 {
-    Super::BeginPlay();
-    StartPreparation();
+	Super::BeginPlay();
+
+	// SpawnZombie 자동 수집
+	TArray<AActor*> FoundActors;
+
+	UGameplayStatics::GetAllActorsOfClass(
+		GetWorld(),
+		AHJ_SpawnZombie::StaticClass(),
+		FoundActors
+	);
+
+	SpawnZombieActors.Empty();
+
+	for (AActor* Actor : FoundActors)
+	{
+		if (AHJ_SpawnZombie* Spawner = Cast<AHJ_SpawnZombie>(Actor))
+		{
+			SpawnZombieActors.Add(Spawner);
+		}
+	}
+
+	StartPreparation();
 }
 
 void AHJ_GameMode::StartPreparation()
 {
-    AHJ_GameState* GS = GetGameState<AHJ_GameState>();
-    if (!GS) return;
+	AHJ_GameState* GS = GetGameState<AHJ_GameState>();
+	if (!GS) return;
 
-    GS->SetBattleState(EBattleState::Preparation);
+	GS->SetBattleState(EBattleState::Preparation);
 
-    FTimerHandle TimerHandle;
-    GetWorldTimerManager().SetTimer(
-        TimerHandle,
-        this,
-        &AHJ_GameMode::StartWave,
-        TimeBetweenWaves,
-        false
-    );
+	GetWorldTimerManager().SetTimer(
+		WaveTimerHandle,
+		this,
+		&AHJ_GameMode::StartWave,
+		TimeBetweenWaves,
+		false
+	);
 }
 
 void AHJ_GameMode::StartWave()
 {
-    AHJ_GameState* GS = GetGameState<AHJ_GameState>();
-    if (!GS) return;
+	AHJ_GameState* GS = GetGameState<AHJ_GameState>();
+	if (!GS || !WaveDataTable) return;
 
-    GS->SetBattleState(EBattleState::InBattle);
+	GS->SetBattleState(EBattleState::InBattle);
 
-    if (SpawnZombieActor)
-    {
-        SpawnZombieActor->StartWave(1); // 최초 웨이브 시작
-    }
+	CurrentWave++;
+	FZombieSpawnRow* Row =
+		WaveDataTable->FindRow<FZombieSpawnRow>(
+			FName(*FString::FromInt(CurrentWave)),
+			TEXT("WaveLookup")
+		);
+
+	if (!Row)
+	{
+		return;
+	}
+	AliveZombieCount = Row->SpawnCount * SpawnZombieActors.Num();
+
+	for (AHJ_SpawnZombie* Spawner : SpawnZombieActors)
+	{
+		if (Spawner)
+		{
+			Spawner->SpawnWave(CurrentWave);
+		}
+	}
 }
 
 void AHJ_GameMode::OnZombieKilled()
 {
+	AliveZombieCount--;
+
+	UE_LOG(LogTemp, Warning, TEXT("Remaining: %d"), AliveZombieCount);
+
+	if (AliveZombieCount <= 0)
+	{
+		EndWave();
+	}
 }
 
 void AHJ_GameMode::EndWave()
 {
-}
-
-void AHJ_GameMode::HandleDefeat()
-{
-    if (bIsDefeatHandled) return;
-    bIsDefeatHandled = true;
-
-    AHJ_GameState* GS = GetGameState<AHJ_GameState>();
-    if (GS)
-    {
-        GS->SetBattleState(EBattleState::Defeat);
-    }
-
-    // 1) 게임 정지(완전 멈춤)
-    UGameplayStatics::SetGamePaused(this, true);
-
-    // 2) 입력 막고 마우스 보여주기 (나중 UI 대비)
-    if (APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0))
-    {
-        PC->SetIgnoreMoveInput(true);
-        PC->SetIgnoreLookInput(true);
-        PC->bShowMouseCursor = true;
-
-        FInputModeUIOnly Mode;
-        PC->SetInputMode(Mode);
-    }
-
-    // 3) 나중에 UI 붙일 훅
-    BP_OnDefeat();
+	StartPreparation();
 }
