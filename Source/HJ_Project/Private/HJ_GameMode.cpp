b849c54f9c77a38091cdff6926ccf519b2e5db61
zazang70-hue <zazang70@gmail.com>
2026-03-01 20:16:44 +0900

    [이재현] 26/03/01
    - 웨이브변경시 UI도 변경되게 변경

--- Source/HJ_Project/Private/HJ_GameMode.cpp
+++ Source/HJ_Project/Private/HJ_GameMode.cpp
@@ -66,6 +66,10 @@ void AHJ_GameMode::StartWave()
 	GS->SetBattleState(EBattleState::InBattle);
 
 	CurrentWave++;
+
+	// GameState에게 바뀐 웨이브 숫자를 알려주고 UI를 업데이트
+	GS->SetWave(CurrentWave);
+
 	FZombieSpawnRow* Row =
 		WaveDataTable->FindRow<FZombieSpawnRow>(
 			FName(*FString::FromInt(CurrentWave)),
